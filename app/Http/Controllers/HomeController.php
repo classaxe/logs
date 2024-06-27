1fa4a171d4448bfa0cab65b6886d4e391cba894c
Martin Francis <martin.f@caslp.ca>
2024-06-27 09:53:04 -0400

    1.0.2 Bug fix for duplication of displayed logs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,30 +16,8 @@ class HomeController extends Controller
     {
         $user = User::getUserByCallsign($callsign);
         $logs = Log::getLogsForUser($user);
-
-        foreach ($logs as $i) {
-            if ($i['mode'] !== 'FT8') {
-                // continue;
-            }
-            $logs[] = [
-                'date' =>   $i['date'],
-                'time' =>   $i['time'],
-                'call' =>   $i['call'],
-                'band' =>   $i['band'],
-                'mode' =>   $i['mode'],
-                'rx' =>     $i['rx'],
-                'tx' =>     $i['tx'],
-                'pwr' =>    $i['pwr'],
-                'qth' =>    $i['qth'],
-                'sp' =>     $i['sp'],
-                'itu' =>    $i['itu'],
-                'gsq' =>    $i['gsq'],
-                'km' =>     $i['km'],
-                'conf' =>   $i['conf']
-            ];
-        }
         $logs = array_reverse($logs);
-//        print "<pre>" . print_r($items[25], true) . "</pre>";
+
 //        print "<pre>" . print_r($logs[25], true) . "</pre>";
 
         return view('callsign', ['user' => $user, 'logs' => $logs]);
--- resources/views/callsign.blade.php
+++ resources/views/callsign.blade.php
@@ -42,49 +42,49 @@
                 @endif
             </header>
             <main>
-<table class="list">
-    <thead>
-        <tr>
-            <td>&nbsp;</td>
-            <th>Date</th>
-            <th>UTC</th>
-            <th>Call</th>
-            <th>Band</th>
-            <th>Mode</th>
-            <th class="num">RX</th>
-            <th class="num">TX</th>
-            <th class="num">Pwr</th>
-            <th>QTH</th>
-            <th>S/P</th>
-            <th>ITU</th>
-            <th>GSQ</th>
-            <th class="num">Km</th>
-            <th>Conf</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($logs as $n=>$log)
-            <tr>
-                <td>{{ $n+1 }}</td>
-                <td class="nowrap">{{ $log['date'] }}</td>
-                <td class="nowrap">{{ $log['time'] }}</td>
-                <td>{{ $log['call'] }}</td>
-                <td><span class="band band{{ $log['band'] }}">{{ $log['band'] }}</span></td>
-                <td>{{ $log['mode'] }}</td>
-                <td class="num">{{ $log['rx'] }}</td>
-                <td class="num">{{ $log['tx'] }}</td>
-                <td class="num">{{ $log['pwr'] }}</td>
-                <td>{{ $log['qth'] }}</td>
-                <td>{{ $log['sp'] }}</td>
-                <td>{{ $log['itu'] }}</td>
-                <td>{{ $log['gsq'] }}</td>
-                <td class="num">{{ number_format($log['km']) }}</td>
-                <td class="r">{{$log['conf']}}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-</main>
+                <table class="list">
+                    <thead>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <th>Date</th>
+                            <th>UTC</th>
+                            <th>Call</th>
+                            <th>Band</th>
+                            <th>Mode</th>
+                            <th class="num">RX</th>
+                            <th class="num">TX</th>
+                            <th class="num">Pwr</th>
+                            <th>QTH</th>
+                            <th>S/P</th>
+                            <th>ITU</th>
+                            <th>GSQ</th>
+                            <th class="num">Km</th>
+                            <th>Conf</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($logs as $n=>$log)
+                            <tr>
+                                <td>{{ $n+1 }}</td>
+                                <td class="nowrap">{{ $log['date'] }}</td>
+                                <td class="nowrap">{{ $log['time'] }}</td>
+                                <td>{{ $log['call'] }}</td>
+                                <td><span class="band band{{ $log['band'] }}">{{ $log['band'] }}</span></td>
+                                <td>{{ $log['mode'] }}</td>
+                                <td class="num">{{ $log['rx'] }}</td>
+                                <td class="num">{{ $log['tx'] }}</td>
+                                <td class="num">{{ $log['pwr'] }}</td>
+                                <td>{{ $log['qth'] }}</td>
+                                <td>{{ $log['sp'] }}</td>
+                                <td>{{ $log['itu'] }}</td>
+                                <td>{{ $log['gsq'] }}</td>
+                                <td class="num">{{ number_format($log['km']) }}</td>
+                                <td class="r">{{$log['conf']}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </main>
         </div>
     </div>
 </div>
