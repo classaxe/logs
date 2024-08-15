e235db5e92fc988cb658eb96ef9346a6b009c24a
Martin Francis <martin@classaxe.com>
2024-08-15 19:03:33 -0400

    1.11.4: Bug fix for missing GSQ listing in Callsigns table on home page

--- resources/views/callsigns.blade.php
+++ resources/views/callsigns.blade.php
@@ -3,14 +3,15 @@
         <h1>QRZ Log and Map Viewer</h1>
         <p>This site enables Radio Amateurs who are <a target="_blank" href="https://shop.qrz.com/collections/subscriptions">XML Subscribers</a>
             at <a target="_blank" href="https://qrz.com">QRZ.com</a> to allow visitors to view their entire log with an
-            interactive Grid Square map without having to log in or create an account at either site. Click on any callsign
-            listed below to view that users logs and map.
+            interactive Grid Square map without having to log in or create an account at either site.
         </p>
         <p>To add your own logs to this system, click  <a href="{{ route('register') }}">Register</a> above to set up a profile.</p>
         <p>You will need to provide your QRZ.com API Key to use this facility, and a moderator will need to approve your
             submission in order to make it active on this site.</p>
         <p>Your logs from QRZ.com will be downloaded to this site automatically every 60 minutes to avoid overburdening
             the QRZ.com servers, but registered users may log in to refresh their logs manually at any time.</p>
+        <h2>Registered Users</h2>
+        <p> Click on any callsign listed below to view that users logs and map.</p>
         <table class="list">
             <thead>
                 <tr>
@@ -31,8 +32,8 @@
                 <td>{{ $u['name'] }}</td>
                 <td>{{ $u['city'] }}</td>
                 <td>{{ $u['sp'] }}</td>
-                <td>{{ $u['name'] }}</td>
                 <td>{{ $u['itu'] }}</td>
+                <td>{{ $u['gsq'] }}</td>
                 <td>{{ $u['log_count'] }}</td>
                 <td><a target="_blank" href="https://www.qrz.com/db/{{ $u['call'] }}">LINK</a></td>
             </tr>
