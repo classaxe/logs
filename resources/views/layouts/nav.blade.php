bf0261787e25c795091c88259ae947f4e31cc7c5
Martin Francis <martin.f@caslp.ca>
2024-09-20 11:37:51 -0400

    1.19.2: Main toolbar button text tweaks

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,23 +3,23 @@
     @auth
         <a href="{{ route('profile.edit') }}"{{
             Route::currentRouteName() === 'profile.edit' ? "class=is-active" : ''
-        }}>Your Profile</a>
+        }}>Profile</a>
         @if (Auth::user()->is_visible)
             <a href="{{ route('logs.page', ['callsign' => Auth::user()->call]) }}"{{
                 Route::currentRouteName() === 'logs.page'
                 && isset(Route::current()->parameters()['callsign'])
                 && Route::current()->parameters()['callsign'] === Auth::user()->call ? "class=is-active" : ''
-            }}>Your Logs</a>
+            }}>Logs</a>
             <a href="{{ route('logs.fetch') }}" id="fetch" title="Reloads your own logs from QRZ.com"{{
                 Route::currentRouteName() === 'logs.fetch' ? "class=is-active" : ''
-            }}>Refresh Your Logs</a>
+            }}>Fetch Logs</a>
         @endif
         <form id="logout" method="POST" action="{{ route('logout') }}">@csrf</form>
-        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout').submit();">{{ __('Log Out') }}</a>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout').submit();">{{ __('Sign Out') }}</a>
     @else
         <a href="{{ route('login') }}"{{
             Route::currentRouteName() === 'login' ? "class=is-active" : ''
-        }}>Log in</a>
+        }}>Sign in</a>
         @if (Route::has('register'))
             <a href="{{ route('register') }}"{{
                 Route::currentRouteName() === 'register' ? "class=is-active" : ''
