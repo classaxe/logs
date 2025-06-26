40d0c229e2b8c46749afdb565313a564adcad9ac
Martin Francis <martin@classaxe.com>
2025-06-26 08:39:25 -0400

    1.50.2: Refreshed park_matches table from import file provided by [Al N3KAE] at cqparks.net - thanks Al

--- app/Models/ParkMatch.php
+++ app/Models/ParkMatch.php
@@ -4,10 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ParkMatch extends Model
 {
     use HasFactory;
     protected $table = 'park_matches';
 
+    public static function ingestDualparksCsv($path, array $prefixes) {
+        DB::statement('DELETE FROM park_matches LIMIT 10000000');
+
+        $imports = [];
+        $fh = fopen($path, 'r');
+        $headers = fgetcsv($fh);
+        while (($row = fgetcsv($fh, 1000, ',')) !== false) {
+            $imports[] = array_combine($headers, $row);
+        }
+        fclose($fh);
+        $data = [];
+        foreach ($imports as $import) {
+            if (empty($import['POTA:']) || empty($import['WWFF:'])) {
+                continue;
+            }
+            if (!in_array(substr($import['POTA:'], 0, 2), $prefixes)) {
+                continue;
+            }
+            $data[] = [
+                'id' =>         null,
+                'ref1' =>       $import['POTA:'],
+                'ref2' =>       $import['WWFF:'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+        ParkMatch::insert($data);
+    }
 }
